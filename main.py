8b6f6fa9e6ceff00e131c003d31c3a59d427a6ab
Muhammad Luthfi Albiansyah <46187943+LLuthfiY@users.noreply.github.com>
2022-01-15 11:11:41 +0700

    new

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import pixel_art_genertor
+from pixel_art_genertor import PixelArtGenerator as pag
 from PIL import Image
 
 
-palette = 'morning.txt'
-image = 'garden2.jpg'
+palette = 'D:\project\Pixel_Art_With_Palette\\purplemorning8.txt'
+image = 'D:\project\Pixel_Art_With_Palette\\red-house.jpg'
+
 
 img = Image.open(image)
-img = pixel_art_genertor.PixelArtGenerator(img)
-# img.read_palette(palette)
+img = pag(img)
+img.read_palette(palette)
 img.pixelate(height=256)
-img.clustering()
+img.clustering(type_color_for_compare=pag.RGB,
+               clustering_method=pag.KMEANSRANDOM)
 img.resize_to_original()
 img.show()
 img.save(image + '-pixel.jpg')
--- pixel_art_genertor.py
+++ pixel_art_genertor.py
@@ -1,12 +1,18 @@
 import numpy as np
-from numpy.core.fromnumeric import reshape
 from skimage import color
 from PIL import Image
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, mean_shift
 import itertools
 
 
 class PixelArtGenerator:
+    LAB = 2
+    RGB = 1
+    HSV = 3
+    LAB_WITH_CUSTOM_DISTANCES_METHOD = 4
+    KMEANS = 1
+    KMEANSRANDOM = 2
+
     def __init__(self, image: Image) -> None:
         self.image = image
         self.palette = None
@@ -37,22 +43,24 @@ class PixelArtGenerator:
     def getImage(self):
         return self.resultImage
 
-    def clustering(self, cluster=7):
+    def clustering(self, cluster=7, type_color_for_compare=1, clustering_method=1):
+        func = self.k_mean
+        if clustering_method == 2:
+            func = self.k_mean_random
         try:
             cluster = len(self.palette)
         except:
             pass
         print('len cluster', cluster)
         img = self.fromImagetoLab(self.resultImage)
-        h, w, _ = img.shape
         img = np.reshape(img, (-1, 3))
-        labels, cluster_centers = self.k_mean(img, cluster)
+        labels, cluster_centers = func(img, cluster)
         print(labels.shape, cluster_centers.shape)
         zeros = np.zeros((labels.shape[0], 3))
 
         try:
             cluster_centers = self.compareClusterCenterWithPallete(
-                cluster_centers, self.palette)
+                cluster_centers, self.palette, type_color_for_compare)
         except:
             pass
         for cluster_center in range(len(cluster_centers)):
@@ -80,29 +88,44 @@ class PixelArtGenerator:
         img_ = KMeans(clusters, random_state=0).fit(img)
         return (img_.labels_, img_.cluster_centers_)
 
-    def euc(self, arr1, arr2):
-        return np.sqrt(abs(arr1[0]-arr2[0])**2 + abs(arr1[1]-arr2[1])**2 + abs(arr1[2]-arr2[2])**2)
+    def k_mean_random(self, img, clusters):
+        img_ = KMeans(clusters, random_state=0, init='random').fit(img)
+        return (img_.labels_, img_.cluster_centers_)
 
-    def compareClusterCenterWithPallete(self, cluster_center, palette):
-        zeros = np.zeros((len(cluster_center), len(palette)))
-        for row in range(len(cluster_center)):
-            for col in range(len(palette)):
-                zeros[row, col] = self.euc(cluster_center[row], palette[col])
-        zeros_reshaped = np.reshape(zeros, (-1, ))
-        zeros_reshaped = sorted(zeros_reshaped)
-        print('cluster', cluster_center)
-        print('palette', palette)
+    def compareClusterCenterWithPallete(self, cluster_center, palette, type_color_for_compare):
+        cluster_center_compare = cluster_center
+        palette_compare = palette
+        func = self.euc
+
+        # change color type based on parameter
+        if (type_color_for_compare == 2):
+            cluster_center_compare = color.rgb2lab(cluster_center)
+            palette_compare = color.rgb2lab(palette)
+        if (type_color_for_compare == 4):
+            cluster_center_compare = color.rgb2lab(cluster_center)
+            palette_compare = color.rgb2lab(palette)
+            func = self.custom_euc_for_lab
+        if (type_color_for_compare == 3):
+            cluster_center_compare = color.rgb2hsv(cluster_center)
+            palette_compare = color.rgb2hsv(np.array(palette))
+
+        distances = [func(x, y)
+                     for x in cluster_center_compare for y in palette_compare]
+        distances_sorted = sorted(distances)
+
         check1 = []
         check2 = []
         pairs = []
-        for el in zeros_reshaped:
-            temp = zip(*np.where(zeros == el))
-            for t in temp:
-                if t[0] not in check1:
-                    if t[1] not in check2:
-                        check1.append(t[0])
-                        check2.append(t[1])
-                        pairs.append(t)
+
+        for index in range(len(distances_sorted)):
+            for i in [x for x in range(len(distances)) if distances[x] == distances_sorted[index]]:
+                div = i//len(cluster_center)
+                mod = i % len(palette)
+                if div not in check1 and mod not in check2:
+                    check1.append(div)
+                    check2.append(mod)
+                    pairs.append([div, mod])
+
         print('pair', pairs)
         for pair in pairs:
             cluster_center[pair[0]] = palette[pair[1]]
@@ -114,3 +137,9 @@ class PixelArtGenerator:
 
     def save(self, text='result-pixelate.jpg'):
         self.resultImage.save(text)
+
+    def euc(self, arr1, arr2):
+        return np.sqrt(abs(arr1[0]-arr2[0])**2 + abs(arr1[1]-arr2[1])**2 + abs(arr1[2]-arr2[2])**2)
+
+    def custom_euc_for_lab(self, arr1, arr2):
+        return np.sqrt(abs(arr1[0]-arr2[0])**2 + abs(arr1[1]-arr2[1])**2) + abs(arr1[2]-arr2[2])**2
